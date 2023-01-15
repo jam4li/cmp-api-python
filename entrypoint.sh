577de98e83426ea3d29e38a33349dc29f2ec70d2
jam4li <ziigm4@gmail.com>
2023-01-15 13:03:22 -0800

    Modify mysql's config in docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]; then
-  echo "Waiting for postgres..."
-
-  while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
-    sleep 0.1
-  done
-
-  echo "PostgreSQL started"
-fi
+while ! nc -z db 3306 ; do
+    echo "Waiting for the MySQL Server"
+    sleep 3
+done
 
 python manage.py migrate
 python manage.py collectstatic --no-input
