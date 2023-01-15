c607b043391482c5aa4ac663d486b0caf28cd935
jam4li <ziigm4@gmail.com>
2023-01-15 13:42:04 -0800

    Add announcement's models

--- announcement/models.py
+++ announcement/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+class Announcements(models.Model):
+    PUBLISH = "publish"
+    DRAFT = "draft"
+    STATUS_CHOICES = (
+        (PUBLISH, _("Publish")),
+        (DRAFT, _("Draft")),
+    )
+
+    title = models.CharField(max_length=255, verbose_name=_('Title'))
+    author = models.CharField(max_length=255, verbose_name=_('Author'))
+    image = models.CharField(max_length=255, verbose_name=_('Image'))
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, verbose_name=_('Status'))
+    summery = models.TextField(max_length=500, verbose_name=_('Summery'))
+    text = models.TextField(max_length=1000, verbose_name=_('Text'))
+    publish_date = models.DateTimeField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    class Meta:
+        verbose_name = _('Announcement')
+        verbose_name_plural = _('Announcements')
+
+    def __str__(self):
+        return self.title
