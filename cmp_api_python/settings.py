b06a1114826fb4c0948eb4e0a9ba32b9d9122b54
jam4li <ziigm4@gmail.com>
2023-01-17 02:20:23 -0800

    Add restframework

--- cmp_api_python/settings.py
+++ cmp_api_python/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'voucher.apps.VoucherConfig',
     'wallet.apps.WalletConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -144,6 +145,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Rest Framework Configuration
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 # Logging Configuration
 
