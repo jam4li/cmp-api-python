14e03e5ba68eac0ef3e6c1b95a63f8d5595588c1
jam4li <ziigm4@gmail.com>
2023-02-02 11:07:04 +0000

    Create coinremitter's gateway in util

---
+++ util/coinremitter.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+import requests
+from django.http import JsonResponse
+
+def create_invoice(amount):
+    api_key = settings.TETHER_API_KEY
+    url = settings.TETHER_GATEWAY_URL
+    expire_time = settings.TETHER_EXPIRE_TIME
+    password = settings.TETHER_PASSWORD
+
+    body = {
+        'api_key': api_key,
+        'password': password,
+        'expire_time': expire_time,
+        'amount': amount,
+        'notify_url': 'https://test.cloudminepro.com/api/trc20/notify/',
+        'success_url': 'https://panel.cloudminepro.com/dashboard',
+        'fail_url': 'https://panel.cloudminepro.com/dashboard',
+    }
+
+    headers = {
+        'Accept': 'application/json',
+    }
+
+    response = requests.post(url, headers=headers, data=body)
+
+    return response.json()
