c78da18b7edfb6b582ccb753dba23d9dcd2b3436
erfan <parsyab1@gmail.com>
2023-03-02 23:38:23 +0330

    completed 2FA OTP logic

--- authentication/api/views.py
+++ authentication/api/views.py
@@ -1,17 +1,10 @@
-from django.shortcuts import render
 from django.utils.decorators import method_decorator
-from django_otp import devices_for_user
+from django_otp import devices_for_user, match_token
 from django_otp.decorators import otp_required
 from django_otp.plugins.otp_totp.models import TOTPDevice
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework import views
-from rest_framework.permissions import AllowAny
-
-
-class TotpTemplateView(views.View):
-    def get(self, request, pk=None, *args, **kwargs):
-        return render(request, 'totp.html', {})
 
 
 class GenerateTOTPSecret(views.APIView):
@@ -24,8 +17,6 @@ class GenerateTOTPSecret(views.APIView):
             device = TOTPDevice.objects.create(
                 user=request.user, name='My Device')
 
-        print(device.config_url)
-
         # Return the TOTP URI to the frontend
         return Response({'totp_uri': device.config_url}, status=status.HTTP_201_CREATED)
 
@@ -33,16 +24,7 @@ class GenerateTOTPSecret(views.APIView):
 class ValidateTOTPToken(views.APIView):
     @method_decorator(otp_required)
     def post(self, request):
-        # Get the TOTP device for the current user
-        devices = devices_for_user(request.user)
-        totp_device = next(
-            (dev for dev in devices if isinstance(dev, TOTPDevice)), None)
+        token = request.data.get('totp_token')
+        matched_devices = match_token(request.user, token)
+        return Response({'is_valid': bool(matched_devices)}, status=status.HTTP_200_OK)
 
-        # Validate the submitted TOTP token
-        is_valid = False
-        if totp_device:
-            token = request.data.get('totp_token')
-            is_valid = totp_device.verify(token)
-
-        # Return a JSON response indicating whether the token is valid or not
-        return Response({'is_valid': is_valid}, status=status.HTTP_200_OK)
--- authentication/urls.py
+++ authentication/urls.py
@@ -3,7 +3,6 @@ from .api.views import TotpTemplateView, GenerateTOTPSecret, ValidateTOTPToken
 
 app_name = 'authentication'
 urlpatterns = [
-    path('totp', TotpTemplateView.as_view(), name='totp'),
     path('generate-totp-secret/', GenerateTOTPSecret.as_view(),
          name='generate_totp_secret'),
     path('validate-totp-token/', ValidateTOTPToken.as_view(),
