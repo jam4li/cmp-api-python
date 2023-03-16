7bb371514111599512891c3924f7f13bb964508a
jam4li <ziigm4@gmail.com>
2023-03-16 22:18:18 +0000

    Split Docker Compose files for development and production environments
    - Create 'docker-compose.dev.yml' for development-specific configurations
    - Create 'docker-compose.prod.yml' for production-specific configurations
    - Update each file with the appropriate services, volumes, and settings
    - Adjust Nginx configuration file paths in respective Docker Compose files

--- manage.py
+++ manage.py
@@ -6,7 +6,6 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cmp_api_python.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
