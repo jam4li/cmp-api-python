99466d7bf46ce305e5f12adf244bcbd0eaf467e6
jam4li <ziigm4@gmail.com>
2023-04-04 18:34:23 +0100

    Separate development and production modes in Google login callback

--- config/settings/base.py
+++ config/settings/base.py
@@ -224,15 +224,7 @@ TETHER_GATEWAY_URL = os.getenv('TETHER_GATEWAY_URL')
 TETHER_EXPIRE_TIME = os.getenv('TETHER_EXPIRE_TIME')
 TETHER_PASSWORD = os.getenv('TETHER_PASSWORD')
 
-# 2FA OTP
-OTP_TOTP_ISSUER = 'cmp'
-# Google OAuth2 settings
-GOOGLE_OAUTH_CLIENT_ID = os.getenv('GOOGLE_OAUTH_CLIENT_ID')
-GOOGLE_OAUTH_CLIENT_SECRET = os.getenv('GOOGLE_OAUTH_CLIENT_SECRET')
-# read this https://www.daimto.com/how-to-get-a-google-access-token-with-curl/
-# 'http://localhost/auth/callback/'
 GOOGLE_OAUTH_REDIRECT_URI = os.getenv('GOOGLE_OAUTH_REDIRECT_URI')
-# GOOGLE_OAUTH_REDIRECT_URI = 'ietf:wg:oauth:2.0:oob'
 
 # celery broker and result
 CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
--- utils/authentication.py
+++ utils/authentication.py
@@ -24,7 +24,7 @@ def create_google_url():
             'openid',
             'https://www.googleapis.com/auth/userinfo.profile',
         ],
-        redirect_uri='http://localhost/auth/callback',
+        redirect_uri=settings.GOOGLE_OAUTH_REDIRECT_URI,
     )
 
     authorization_url, state = flow.authorization_url()
@@ -43,34 +43,45 @@ def callback_google(request):
             'https://www.googleapis.com/auth/userinfo.profile',
         ],
         state=request.GET['state'],
-        redirect_uri='http://localhost/auth/callback',
+        redirect_uri=settings.GOOGLE_OAUTH_REDIRECT_URI,
     )
 
     # Exchange the authorization code for an access token and a refresh token
     authorization_response = request.build_absolute_uri()
     flow.fetch_token(authorization_response=authorization_response)
 
-    # Get the user's credentials (access token and refresh token)
-    credentials = flow.credentials
+    # Development mode
+    if os.environ['OAUTHLIB_INSECURE_TRANSPORT'] == '1':
+        # Get the user's credentials (access token and refresh token)
+        credentials = flow.credentials
 
-    # Check if the _id_token attribute exists in the credentials object
-    if hasattr(credentials, '_id_token'):
-        # Get the user's ID token
-        id_token_str = credentials._id_token
+        # Check if the _id_token attribute exists in the credentials object
+        if hasattr(credentials, '_id_token'):
+            # Get the user's ID token
+            id_token_str = credentials._id_token
+        else:
+            # Raise an exception or handle the error as needed
+            raise ValueError("ID token not found in the credentials object")
+
+        # Convert the ID token to bytes
+        id_token_bytes = id_token_str.encode('utf-8')
+
+        # Use the following line in development only
+        id_info = jwt.decode(
+            id_token_bytes,
+            options={"verify_signature": False},
+        )
+
+        return id_info
+
+    # Production or Staging mode
     else:
-        # Raise an exception or handle the error as needed
-        raise ValueError("ID token not found in the credentials object")
+        # Get the user's credentials (access token and refresh token)
+        credentials = Credentials.from_authorized_user_info(
+            info=flow.credentials.to_json(),
+        )
 
-    # Verify the ID token and fetch the user's profile information
-    # id_info = id_token.verify_oauth2_token(credentials.id_token, Request())
+        # Verify the ID token and fetch the user's profile information
+        id_info = id_token.verify_oauth2_token(credentials.id_token, Request())
 
-    # Convert the ID token to bytes
-    id_token_bytes = id_token_str.encode('utf-8')
-
-    # Use the following line in development only
-    id_info = jwt.decode(
-        id_token_bytes,
-        options={"verify_signature": False},
-    )
-
-    return id_info
+        return id_info
