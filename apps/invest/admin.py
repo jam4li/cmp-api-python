e8824ba18fae35db3bd53d1b5000f5097aeece95
jam4li <ziigm4@gmail.com>
2023-04-06 02:41:48 +0100

    Modify admin.py in invest for better performance
    - Making calculated_at and deleted_at nullable in Invest model
    - Adding migration script for Invest in utils

--- apps/invest/admin.py
+++ apps/invest/admin.py
@@ -5,4 +5,37 @@ from .models import Invest
 # Register your models here.
 
 
-admin.site.register(Invest)
+class InvestAdmin(admin.ModelAdmin):
+    list_select_related = True
+    list_per_page = 50
+    raw_id_fields = (
+        'user',
+    )
+    autocomplete_fields = [
+        'package',
+    ]
+    search_fields = [
+        'user__email',
+        'package__name',
+    ]
+
+    fields = (
+        'user',
+        'package',
+        'invest',
+        'total_invest',
+        'profit',
+        'payout_binary_status',
+        'payout_direct_status',
+        'finished',
+        'calculated_at',
+        'deleted_at',
+    )
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        qs = qs.select_related('user')
+        return qs
+
+
+admin.site.register(Invest, InvestAdmin)
--- apps/invest/models.py
+++ apps/invest/models.py
@@ -46,8 +46,14 @@ class Invest(BaseModel):
         verbose_name=_('Finished'),
     )
     # TODO: calculated_at default = now()
-    calculated_at = models.DateTimeField()
-    deleted_at = models.DateTimeField()
+    calculated_at = models.DateTimeField(
+        blank=True,
+        null=True,
+    )
+    deleted_at = models.DateTimeField(
+        blank=True,
+        null=True,
+    )
 
     class Meta:
         db_table = 'invests'
