6fe2c3a87571abcfcaccda8ab942bf937ef7aaa2
jam4li <ziigm4@gmail.com>
2023-04-07 13:01:35 +0100

    Modify active_packages and balance in UserDashboard API

--- apps/users/api/views.py
+++ apps/users/api/views.py
@@ -2,14 +2,27 @@ from rest_framework import views
 from rest_framework.response import Response
 
 from apps.users.models import User
+from apps.invest.models import Invest
+from apps.wallet.models import Wallet
 # from .serializers import AnnouncementListSerializer
 
 
 class UserDashboardAPIView(views.APIView):
     def get(self, request, format=None):
+        user = self.request.user
+
+        # Calculate active packages
+        active_packages = Invest.objects.filter(user=user).count()
+
+        # Calculate balance
+        balance = 0
+        wallet_list = Wallet.objects.filter(user=user)
+        for wallet in wallet_list:
+            balance += wallet.balance
+
         data = {
-            "active_packages": "Pending",
-            "balance": "Pending",
+            "active_packages": active_packages,
+            "balance": balance,
             "direct_invited": "Pending",
             "team": "Pending",
         }
