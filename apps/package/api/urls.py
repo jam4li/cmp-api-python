af02259a6d71730cb3ac5915a7207127c13108ef
jam4li <ziigm4@gmail.com>
2023-04-10 01:08:47 +0100

    Modify PackageList URL in application
    - Updating the PackageList URL in the application to ensure that the correct API endpoint is used for retrieving package information

--- apps/package/api/urls.py
+++ apps/package/api/urls.py
@@ -4,6 +4,6 @@ from .views import PackageListAPIView, PackageBuyAPIView
 
 app_name = 'package'
 urlpatterns = [
-    path('', PackageListAPIView.as_view(), name='list'),
+    path('list/', PackageListAPIView.as_view(), name='list'),
     path('buy/', PackageBuyAPIView.as_view(), name='buy'),
 ]
