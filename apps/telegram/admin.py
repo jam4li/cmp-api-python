d5762ec03b05543593843a83b8a9dec956b09fd2
jam4li <ziigm4@gmail.com>
2023-07-08 15:25:48 +0330

    Add TelegramConfig, TelegramLanguage, TelegramUser, TelegramFAQ
    - TelegramConfig to save the support's Telegram username, allowing users to contact support easily
    - TelegramLanguage for supporting multiple languages
    - TelegramUser to store information about users in Telegram Bot
    - TelegramFAQ allows the management of frequently asked questions

--- apps/telegram/admin.py
+++ apps/telegram/admin.py
@@ -2,24 +2,154 @@ from django.contrib import admin
 
 from apps.base.admin import BaseAdmin
 
-from .models import Educate, EducateContent
+from .models import (
+    TelegramConfig,
+    TelegramLanguage,
+    TelegramUser,
+    TelegramNewsFile,
+    TelegramNews,
+    TelegramEducate,
+    TelegramEducateContent,
+    TelegramOfficeFile,
+    TelegramOffice,
+    TelegramFAQ,
+)
 
 # Register your models here.
 
 
-class EducateContentInline(admin.TabularInline):
-    model = EducateContent
+class TelegramLanguageAdmin(BaseAdmin):
+    fields = (
+        'language_code',
+    ) + BaseAdmin.fields
+
+    readonly_fields = BaseAdmin.readonly_fields
+
+
+admin.site.register(TelegramLanguage, TelegramLanguageAdmin)
+
+
+class TelegramUserAdmin(BaseAdmin):
+    model = TelegramUser
+    fields = (
+        'user_id',
+        'language',
+    ) + BaseAdmin.fields
+
+    readonly_fields = BaseAdmin.readonly_fields
+
+
+admin.site.register(TelegramUser, TelegramUserAdmin)
+
+
+class TelegramNewsFileInline(admin.TabularInline):
+    model = TelegramNewsFile
+    fields = (
+        'file',
+    )
     extra = 1
 
 
-class EducateAdmin(BaseAdmin):
+class TelegramNewsAdmin(BaseAdmin):
     fields = (
-        'name',
-    )
+        'language',
+        'title',
+        'description',
+    ) + BaseAdmin.fields
+
+    readonly_fields = BaseAdmin.readonly_fields
 
     inlines = [
-        EducateContentInline,
+        TelegramNewsFileInline,
     ]
 
 
-admin.site.register(Educate, EducateAdmin)
+admin.site.register(TelegramNews, TelegramNewsAdmin)
+
+
+class TelegramEducateContentInline(admin.TabularInline):
+    model = TelegramEducateContent
+    fields = (
+        'title',
+        'description',
+        'file',
+    )
+    extra = 1
+
+
+class TelegramEducateAdmin(BaseAdmin):
+    fields = (
+        'language',
+        'name',
+    ) + BaseAdmin.fields
+
+    readonly_fields = BaseAdmin.readonly_fields
+
+    inlines = [
+        TelegramEducateContentInline,
+    ]
+
+    def last_5_titles(self, obj):
+        titles = obj.telegrameducatecontent_set.order_by(
+            '-id',
+        )[:5].values_list('title', flat=True)
+        return ", ".join(titles)
+
+    list_display = (
+        'name',
+        'last_5_titles',
+        'language',
+    )
+
+
+admin.site.register(TelegramEducate, TelegramEducateAdmin)
+
+
+class TelegramOfficeFileInline(admin.TabularInline):
+    model = TelegramOfficeFile
+    fields = (
+        'file',
+    )
+    extra = 1
+
+
+class TelegramOfficeAdmin(BaseAdmin):
+    fields = (
+        'title',
+        'location_latitude',
+        'location_longitude',
+        'location_address',
+    ) + BaseAdmin.fields
+
+    readonly_fields = BaseAdmin.readonly_fields
+
+    inlines = [
+        TelegramOfficeFileInline,
+    ]
+
+
+admin.site.register(TelegramOffice, TelegramOfficeAdmin)
+
+
+class TelegramFAQAdmin(BaseAdmin):
+    fields = (
+        'language',
+        'question',
+        'answer',
+    ) + BaseAdmin.fields
+
+    readonly_fields = BaseAdmin.readonly_fields
+
+
+admin.site.register(TelegramFAQ, TelegramFAQAdmin)
+
+
+class TelegramConfigAdmin(BaseAdmin):
+    fields = (
+        'support_username',
+    ) + BaseAdmin.fields
+
+    readonly_fields = BaseAdmin.readonly_fields
+
+
+admin.site.register(TelegramConfig, TelegramConfigAdmin)
--- apps/telegram/models.py
+++ apps/telegram/models.py
@@ -1,10 +1,126 @@
 from django.utils.translation import gettext_lazy as _
 
-from apps.base.models import models, BaseModel
+from apps.base.models import models, BaseModel, BaseAsyncSingletonModel
 # Create your models here.
 
 
-class Educate(BaseModel):
+class TelegramConfig(BaseAsyncSingletonModel):
+    support_username = models.CharField(
+        max_length=255,
+        verbose_name=_('Support username')
+    )
+
+    class Meta:
+        verbose_name = _('Config')
+        verbose_name_plural = _('Config')
+
+    def __str__(self):
+        return 'Primary Config'
+
+
+class TelegramLanguage(BaseModel):
+    ENGLISH = "en"
+    ARABIC = "ar"
+    PERSIAN = "fa"
+    TURKISH = "tr"
+    RUSSIAN = "ru"
+    LANGUAGE_CODE_CHOICES = (
+        (ENGLISH, _("English")),
+        (ARABIC, _("Arabic")),
+        (PERSIAN, _("Persian")),
+        (TURKISH, _("Turkish")),
+        (RUSSIAN, _("Russian")),
+    )
+
+    language_code = models.CharField(
+        choices=LANGUAGE_CODE_CHOICES,
+        max_length=4,
+        verbose_name=_('Language code'),
+    )
+
+    class Meta:
+        verbose_name = _('Language')
+        verbose_name_plural = _('Languages')
+
+    def __str__(self):
+        return self.get_language_code_display()
+
+
+def get_default_language():
+    return TelegramLanguage.objects.get(language_code='en')
+
+
+class TelegramUser(BaseModel):
+    user_id = models.CharField(
+        max_length=255,
+        verbose_name=_('User id'),
+    )
+    language = models.ForeignKey(
+        TelegramLanguage,
+        on_delete=models.SET_DEFAULT,
+        default=get_default_language,
+    )
+
+    class Meta:
+        verbose_name = _('Telegram User')
+        verbose_name_plural = _('Telegram Users')
+
+    def __str__(self):
+        return self.user_id
+
+
+class TelegramNews(BaseModel):
+    language = models.ForeignKey(
+        TelegramLanguage,
+        on_delete=models.CASCADE,
+        verbose_name=_('Language'),
+    )
+    title = models.CharField(
+        max_length=255,
+        verbose_name=_('Title'),
+    )
+    description = models.TextField(
+        max_length=200,
+        blank=True,
+        null=True,
+        verbose_name=_('Description'),
+    )
+
+    class Meta:
+        verbose_name = _('News')
+        verbose_name_plural = _('News')
+
+    def __str__(self):
+        return self.title
+
+
+class TelegramNewsFile(BaseModel):
+    file = models.FileField(
+        upload_to='telegram/',
+        blank=True,
+        null=True,
+        verbose_name=_('File'),
+    )
+    news = models.ForeignKey(
+        TelegramNews,
+        on_delete=models.CASCADE,
+        verbose_name=_('News'),
+    )
+
+    class Meta:
+        verbose_name = _('News file')
+        verbose_name_plural = _('News files')
+
+    def __str__(self):
+        return str(self.file)
+
+
+class TelegramEducate(BaseModel):
+    language = models.ForeignKey(
+        TelegramLanguage,
+        on_delete=models.CASCADE,
+        verbose_name=_('Language'),
+    )
     name = models.CharField(
         max_length=255,
         verbose_name=_('Name'),
@@ -18,9 +134,9 @@ class Educate(BaseModel):
         return self.name
 
 
-class EducateContent(BaseModel):
+class TelegramEducateContent(BaseModel):
     educate = models.ForeignKey(
-        Educate,
+        TelegramEducate,
         on_delete=models.CASCADE,
         verbose_name=_('Educate'),
     )
@@ -35,7 +151,7 @@ class EducateContent(BaseModel):
         verbose_name=_('Description'),
     )
     file = models.FileField(
-        upload_to='educate/',
+        upload_to='telegram/educate/',
         blank=True,
         null=True,
         verbose_name=_('File'),
@@ -47,3 +163,76 @@ class EducateContent(BaseModel):
 
     def __str__(self):
         return self.description
+
+
+class TelegramOffice(BaseModel):
+    title = models.CharField(
+        max_length=255,
+        verbose_name=_('Title'),
+    )
+    location_latitude = models.CharField(
+        max_length=15,
+        blank=True,
+        null=True,
+    )
+    location_longitude = models.CharField(
+        max_length=15,
+        blank=True,
+        null=True,
+    )
+    location_address = models.TextField(
+        max_length=255,
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = _('Office')
+        verbose_name_plural = _('Offices')
+
+    def __str__(self):
+        return self.title
+
+
+class TelegramOfficeFile(BaseModel):
+    file = models.FileField(
+        upload_to='telegram/office/',
+        blank=True,
+        null=True,
+        verbose_name=_('File'),
+    )
+    office = models.ForeignKey(
+        TelegramOffice,
+        on_delete=models.CASCADE,
+        verbose_name=_('Office'),
+    )
+
+    class Meta:
+        verbose_name = _('Office file')
+        verbose_name_plural = _('Office files')
+
+    def __str__(self):
+        return str(self.file)
+
+
+class TelegramFAQ(BaseModel):
+    language = models.ForeignKey(
+        TelegramLanguage,
+        on_delete=models.CASCADE,
+        verbose_name=_('Language'),
+    )
+    question = models.CharField(
+        max_length=100,
+        verbose_name=_('Question'),
+    )
+    answer = models.TextField(
+        max_length=500,
+        verbose_name=_('Answer'),
+    )
+
+    class Meta:
+        verbose_name = _('FAQ')
+        verbose_name_plural = _('FAQs')
+
+    def __str__(self):
+        return self.question
