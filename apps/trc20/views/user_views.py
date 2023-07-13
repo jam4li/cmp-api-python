b22c6a76b5620c6bb78161d443a44c22b2e1e056
jam4li <ziigm4@gmail.com>
2023-07-13 14:29:03 +0330

    Add HTTP_200_OK in Trc20NotifyGatewayAPIView

--- apps/trc20/views/user_views.py
+++ apps/trc20/views/user_views.py
@@ -1,6 +1,7 @@
 from rest_framework import views
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from rest_framework.status import HTTP_200_OK
 from django.utils.translation import gettext
 
 from utils.response import ApiResponse
