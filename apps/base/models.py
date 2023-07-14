670f9f6776120d32272cb2cfbe4690280edfb51b
jam4li <ziigm4@gmail.com>
2023-07-14 13:44:47 +0330

    Modify created_at and updated_at fields in base models.py

--- apps/base/models.py
+++ apps/base/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
@@ -6,13 +7,11 @@ from django.utils.translation import gettext_lazy as _
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(
-        auto_now_add=True,
         blank=True,
         null=True,
         verbose_name=_('Created at'),
     )
     updated_at = models.DateTimeField(
-        auto_now=True,
         blank=True,
         null=True,
         verbose_name=_('Updated at'),
