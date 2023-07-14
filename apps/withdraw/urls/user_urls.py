baff71c662f95d820c8fdae3b8649bb690b6f88e
jam4li <ziigm4@gmail.com>
2023-07-14 17:41:31 +0330

    Add WithdrawCreateAPIView and WithdrawDetailAPIView
    - The WithdrawCreateAPIView is responsible for creating withdraws based on the user, wallet type, and balance
    - The WithdrawDetailAPIView provides detailed information about the available wallet types for withdrawals and the activation status

--- apps/withdraw/urls/user_urls.py
+++ apps/withdraw/urls/user_urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from apps.withdraw.views.user_views import WithdrawListAPIView
+from apps.withdraw.views.user_views import (
+    WithdrawListAPIView,
+    WithdrawDetailAPIView,
+    WithdrawCreateAPIView,
+)
 
 app_name = 'withdraw_user'
 
 urlpatterns = [
     path('list/', WithdrawListAPIView.as_view(), name='list'),
+    path('detail/', WithdrawDetailAPIView.as_view(), name='detail'),
+    path('create/', WithdrawCreateAPIView.as_view(), name='create'),
 ]
--- apps/withdraw/views/user_views.py
+++ apps/withdraw/views/user_views.py
@@ -1,9 +1,13 @@
+from django.utils import timezone
+
 from rest_framework import views
 from rest_framework.response import Response
 
 from utils.response import ApiResponse
 
 from apps.withdraw.models import Withdraw
+from apps.wallet.models import Wallet
+
 from apps.withdraw.serializers.user_serializers import WithdrawListSerializer
 
 
@@ -27,6 +31,92 @@ class WithdrawListAPIView(views.APIView):
             success=True,
             code=200,
             data=serializer.data,
+            message='Data retrieved successfully'
+        )
+
+        return Response(success_response)
+
+
+class WithdrawDetailAPIView(views.APIView):
+    def get(self, request, format=None):
+        wallet_types = [
+            {
+                'wallet_type': choice[0],
+                'display_name': choice[1]
+            }
+            for choice in Withdraw.WALLET_TYPE_CHOICES
+        ]
+
+        data = {
+            'wallet_types': wallet_types,
+            'is_active': True,
+        }
+
+        success_response = ApiResponse(
+            success=True,
+            code=200,
+            data=data,
+            message='Data retrieved successfully'
+        )
+
+        return Response(success_response)
+
+
+class WithdrawCreateAPIView(views.APIView):
+    def post(self, request, format=None):
+        user = self.request.user
+        wallet_address = self.request.data['wallet_address']
+        wallet_type = self.request.data['wallet_type']
+        amount = self.request.data['amount']
+
+        amount = float(amount)
+
+        # Calculate fee
+        fee = 0
+
+        if wallet_type == 'commission':
+            fee = (10 * amount) / 100
+
+        elif wallet_type == 'profit':
+            fee = 2
+
+        # Check wallet balance based on the wallet_type
+        wallet = Wallet.objects.get(
+            user=user,
+            type=wallet_type,
+        )
+
+        wallet_balance = float(wallet.balance)
+
+        if wallet_balance < amount + fee:
+            response = ApiResponse(
+                success=False,
+                code=402,
+                error={
+                    'code': 'insufficient balance',
+                    'detail': 'Insufficient balance in wallet',
+                }
+            )
+
+            return Response(response)
+
+        Withdraw.objects.create(
+            user=user,
+            amount=amount,
+            fee=fee,
+            wallet_address=wallet_address,
+            wallet_type=wallet_type,
+            status='pending',
+            created_at=timezone.now(),
+        )
+
+        wallet.balance = wallet_balance - (amount + fee)
+        wallet.updated_at = timezone.now()
+        wallet.save()
+
+        success_response = ApiResponse(
+            success=True,
+            code=200,
             message='Data retrieved successfully'
         )
 
