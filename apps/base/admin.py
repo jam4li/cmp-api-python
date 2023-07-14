51a810b2f6a59141bba0e13510e33054c12757bc
jam4li <ziigm4@gmail.com>
2023-07-14 13:43:54 +0330

    Add save_model method in base admin
    - This methods handles the automatic filling of created_at and updated_at fields when saving models in the Django admin interface

--- apps/base/admin.py
+++ apps/base/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils import timezone
 from django.template.defaultfilters import date as admin_dateformat
 
 # Register your models here.
@@ -10,6 +11,13 @@ class BaseAdmin(admin.ModelAdmin):
 
     def custom_updated_at(self, obj):
         return admin_dateformat(obj.updated_at, 'Y-m-d H:i:s')
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_at = timezone.now()
+        obj.updated_at = timezone.now()
+
+        super().save_model(request, obj, form, change)
 
     fields = (
         'id',
