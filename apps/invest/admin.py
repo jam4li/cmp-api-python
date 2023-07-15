10e2600d1be3081cc1e5d8468fa7716963e56b41
jam4li <ziigm4@gmail.com>
2023-07-16 00:04:14 +0330

    Add save_model method to InvestAdmin for creating Invest objects in the admin panel

--- apps/invest/admin.py
+++ apps/invest/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
+from django.utils import timezone
 
 from apps.base.admin import BaseAdmin
 
 from .models import Invest
+from apps.network.models import Network
+from apps.users.models import User, UserProfile
+from apps.package.models import Package
 
 # Register your models here.
 
@@ -43,6 +47,45 @@ class InvestAdmin(BaseAdmin):
         qs = super().get_queryset(request)
         qs = qs.select_related('user')
         return qs
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            user = form.cleaned_data['user']
+            package = form.cleaned_data['package']
+            total_invest = form.cleaned_data['total_invest']
+
+            user_profile = UserProfile.objects.get(user=user)
+
+            package_price = package.price
+
+            network = Network.objects.get(user=user)
+
+            # Calculate and update total_invest
+            total_invest = 0
+            total_invest += package.price
+            invest_list = Invest.objects.filter(user=user)
+
+            for invest in invest_list:
+                total_invest += invest.invest
+
+            network.invest = total_invest
+            network.last_invest = package.price
+            network.save()
+
+            if user_profile.package:
+                if package_price > user_profile.package.price:
+                    user_profile.package = package
+                    user_profile.save()
+
+            else:
+                user_profile.package = package
+                user_profile.save()
+
+            obj.invest = package.price
+            obj.total_invest = total_invest
+            obj.created_at = timezone.now()
+
+        super().save_model(request, obj, form, change)
 
 
 admin.site.register(Invest, InvestAdmin)
