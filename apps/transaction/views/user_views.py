2dcf9fda40269c0efe4909b94b4e7b40bd602cc5
jam4li <ziigm4@gmail.com>
2023-07-24 16:34:46 +0330

    Add docstring in TransactionListAPIView

--- apps/transaction/views/user_views.py
+++ apps/transaction/views/user_views.py
@@ -8,6 +8,52 @@ from apps.transaction.serializers.user_serializers import TransactionListSeriali
 
 
 class TransactionListAPIView(views.APIView):
+    """
+    API view to fetch a list of transactions for the authenticated user.
+
+    This view handles a GET request to retrieve a list of transactions for the authenticated user.
+
+    Returns:
+        - A JSON response containing a list of transactions for the user:
+            - Transaction List (list): List of transactions, each represented as an object with transaction details.
+                - "amount" (double): The amount of transaction.
+                - "type" (str): profit binary direct
+                - "description" (str): The description about the transaction
+                - "updated_at" (datetime): When the transaction has been updated
+
+    Note:
+        - The API expects a GET request to fetch a list of transactions for the authenticated user.
+        - The authenticated user is considered the account holder for whom the transactions are retrieved.
+        - The API queries the database for transactions that belong to the user.
+        - The transactions are ordered by their creation date in ascending order.
+        - The serializer used to format the transaction list may vary based on the TransactionListSerializer used.
+        - The transaction list provides information about each transaction, such as the transaction type, amount, etc.
+
+    Example Response:
+    ```
+    HTTP 200 OK
+    {
+        "success": true,
+        "code": 200,
+        "data": [
+            {
+                "amount": 100.000,
+                "type": "profit",
+                "description": "Daily profit of 100.000$",
+                "updated_at": "2023-07-22 03:30:00" 
+            },
+            {
+                "amount": 200.000,
+                "type": "profit",
+                "description": "Daily profit of 200.000$",
+                "updated_at": "2023-07-22 03:30:00" 
+            },
+        ],
+        "message": "Data retrieved successfully"
+    }
+    ```
+    """
+
     def get(self, request, format=None):
         user = self.request.user
 
