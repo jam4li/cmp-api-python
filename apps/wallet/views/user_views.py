de766ed05f86f2bb2504510c2ea103cc7717c868
jam4li <ziigm4@gmail.com>
2023-07-24 15:14:11 +0330

    Add docstring in WalletListAPIView

--- apps/wallet/views/user_views.py
+++ apps/wallet/views/user_views.py
@@ -8,6 +8,52 @@ from apps.wallet.serializers.user_serializers import WalletListSerializer
 
 
 class WalletListAPIView(views.APIView):
+    """
+    API view to fetch a list of wallets associated with the authenticated user.
+
+    This view handles a GET request to retrieve a list of wallets associated with the authenticated user.
+
+    Returns:
+        - A JSON response containing a list of wallets for the user:
+            - Wallet List (list): List of wallets, each represented as an object with wallet details.
+                - "title" (str): (e.g., 'Deposit Wallet')
+                - "type" (str): deposit commission profit eit voucher
+                - "access_type" (str): user company accounting
+                - "balance" (double): Balance of wallet in double
+
+    Note:
+        - The API expects a GET request to fetch a list of wallets associated with the authenticated user.
+        - The authenticated user is considered the account holder for whom the wallets are retrieved.
+        - The API queries the database for wallets that belong to the user.
+        - The serializer used to format the wallet list may vary based on the WalletListSerializer used.
+        - The wallet list provides information about each wallet, such as the wallet type, balance, etc.
+
+    Example Response:
+    ```
+    HTTP 200 OK
+    {
+        "success": true,
+        "code": 200,
+        "data": [
+            {
+                "title": "Profit Wallet",
+                "type": "profit",
+                "access_type": "user",
+                "balance": 100.000,
+            },
+            {
+                "title": "EIT Wallet",
+                "type": "eit",
+                "access_type": "user",
+                "balance": 1000.000,
+            },
+            ...
+        ],
+        "message": "Data retrieved successfully"
+    }
+    ```
+    """
+
     def get(self, request, format=None):
         user = self.request.user
 
