00ea8ad32467da018076dda03f8545be23718973
jam4li <ziigm4@gmail.com>
2023-07-24 12:32:20 +0330

    Add docstring in users views
    - Enhancing docstring in UserDashboardAPIView, UserDetailAPIView, UserCreateAPIView, UserReferralDetailAPIView, GoogleLogin, GoogleCallback, Logout

--- apps/users/views/user_views.py
+++ apps/users/views/user_views.py
@@ -17,6 +17,28 @@ from apps.network.models import Network
 
 
 class UserDashboardAPIView(views.APIView):
+    """
+    API view to fetch user's dashboard data.
+
+    This view handles a GET request to retrieve the user's dashboard data.
+
+    Returns:
+        - A JSON response containing the user's dashboard data:
+            - "active_packages" (int): The number of active investment packages the user has.
+            - "balance" (float): The total balance across all wallets, considering different wallet types:
+                - The 'eit' wallet balance is divided by 4.
+                - The other wallet types (e.g., 'deposit') are considered as is.
+            - "direct_invited" (int): The count of users directly invited by the user (referrals).
+            - "team" (int): The total size of the user's team in the binary tree structure.
+
+    Note:
+        - The "active_packages" count is calculated based on the Invest model, where 'finished' is False.
+        - The "balance" is calculated by summing up the balances of all wallets belonging to the user.
+        - For 'eit' wallets, the balance is divided by 4, while other wallet types remain as is.
+        - The "direct_invited" count is based on the number of users in the Network model with the user as the referrer.
+        - The "team" size is determined by summing up the counts of left and right children nodes in the binary tree.
+    """
+
     def get(self, request, format=None):
         user = self.request.user
 
@@ -62,6 +84,20 @@ class UserDashboardAPIView(views.APIView):
 
 
 class UserDetailAPIView(views.APIView):
+    """
+    API view to fetch user's email details.
+
+    This view handles a GET request to retrieve the user's email details.
+
+    Returns:
+        - A JSON response containing the user's email:
+            - "email" (str): The user's email address without the domain part.
+
+    Note:
+        - The user's email is extracted from the authenticated user's data.
+        - The email domain part is omitted (e.g., '@gmail.com'), and only the local part is included in the response.
+    """
+
     def get(self, request, format=None):
         user = self.request.user
         email = user.email.split('@')[0]
@@ -80,6 +116,29 @@ class UserDetailAPIView(views.APIView):
 
 
 class UserCreateAPIView(views.APIView):
+    """
+    API view for creating a new user profile and generating a Gmail login link.
+
+    This view handles a POST request to create a new user profile and generate a Gmail login link based on provided data.
+
+    Parameters:
+        - 'referrer' (str): The referrer's unique code for the user inviting the new user.
+        - 'side' (str): The side on which the new user will be placed in the binary tree structure.
+
+    Returns:
+        - A JSON response containing the Gmail login link:
+            - "url" (str): The generated Gmail login link for the new user.
+
+    Note:
+        - The API is designed to create a new user profile and generate a Gmail login link for the user.
+        - The 'referrer' parameter is used to identify the referrer user in the database.
+        - The 'side' parameter specifies the side (left or right) on which the new user will be placed in the binary tree.
+        - The API verifies the existence of the referrer user based on the provided referrer code.
+        - If the referrer user is not found in the database, the API returns an error response with a 404 status code.
+        - If the referrer is found, the API creates a Gmail login link using the provided 'side' and referrer code.
+        - The generated Gmail login link is returned in the response.
+    """
+
     permission_classes = [AllowAny, ]
 
     def post(self, request, format=None):
@@ -114,6 +173,21 @@ class UserCreateAPIView(views.APIView):
 
 
 class UserReferralDetailAPIView(views.APIView):
+    """
+    API view to fetch the referral code of the authenticated user.
+
+    This view handles a GET request to retrieve the referral code of the authenticated user.
+
+    Returns:
+        - A JSON response containing the user's referral code:
+            - "referrer_code" (str): The unique referral code associated with the user.
+
+    Note:
+        - The API fetches the authenticated user's profile using the provided user data.
+        - The "referrer_code" represents the unique referral code associated with the user.
+        - The referral code is used for inviting new users and tracking referrals in the system.
+    """
+
     def get(self, request, format=None):
         user = self.request.user
         user_profile = UserProfile.objects.get(user=user)
@@ -133,6 +207,24 @@ class UserReferralDetailAPIView(views.APIView):
 
 
 class GoogleLogin(views.APIView):
+    """
+    API view to generate a Gmail login link.
+
+    This view handles a GET request to generate a Gmail login link for user authentication.
+
+    Returns:
+        - A JSON response containing the Gmail login link:
+            - "url" (str): The generated Gmail login link for user authentication.
+
+    Note:
+        - The API generates a Gmail login link to allow users to authenticate using their Gmail accounts.
+        - The link is used to initiate the Gmail login process.
+        - Users will be redirected to the Gmail login page, where they can log in with their Gmail credentials.
+        - Upon successful authentication, users can grant permission to the website.
+        - The website can then use the provided Gmail access token to access the user's Gmail data (if authorized).
+
+    """
+
     permission_classes = [AllowAny, ]
 
     def get(self, request):
@@ -141,6 +233,41 @@ class GoogleLogin(views.APIView):
 
 
 class GoogleCallback(views.APIView):
+    """
+    API view to handle the callback from Google OAuth2 authentication.
+
+    This view handles a GET request containing the callback data from Google OAuth2 authentication.
+    The callback data is used to get_or_create a user and generate an authentication token.
+
+    Parameters:
+        - N/A (Note: The callback data is extracted from the request object directly.)
+
+    Returns:
+        - A redirect response to the user dashboard:
+            - The user's dashboard is accessible through a frontend URL.
+            - The redirect URL includes the authentication token and user email as query parameters.
+
+    Note:
+        - The API expects a callback from Google OAuth2 authentication, which provides user details such as email and name.
+        - The API uses the received data to get_or_create a user in the system.
+        - The 'side' and 'referrer_code' parameters, if present, are used to place the new user in the binary tree structure.
+        - After creating the new user, the API generates an authentication token for the user.
+        - The authentication token is included in the redirect URL to allow seamless authentication on the frontend.
+        - The redirect URL leads to the user's dashboard, which is accessible through the frontend application.
+
+    Redirect URL Format:
+    ```
+    https://FRONT_END_URL/dashboard/?token=<authentication_token>&email=<user_email>
+    ```
+
+    (Note: Replace <authentication_token> and <user_email> with the actual token and email data)
+
+    Example Redirect URL:
+    ```
+    https://cm-enterprise.com/dashboard/?token=your_auth_token_here&email=user@example.com
+    ```
+    """
+
     permission_classes = [AllowAny, ]
 
     def get(self, request):
@@ -173,6 +300,42 @@ class GoogleCallback(views.APIView):
 
 
 class Logout(views.APIView):
+    """
+    API view to handle user logout and revoke the authentication token.
+
+    This view handles a GET request to revoke the authentication token of the authenticated user.
+
+    Parameters:
+        - N/A (Note: The authentication token is revoked based on the authenticated user.)
+
+    Returns:
+        - A response indicating the result of the logout process:
+            - If the token is successfully revoked, a success message is returned with a 200 status code.
+            - If the authenticated user is not found or has no associated token, an error message is returned with a 400 status code.
+
+    Note:
+        - The API expects a GET request to revoke the authentication token of the authenticated user.
+        - The authentication token is obtained based on the authenticated user.
+        - If a valid token is found for the user, it is deleted to revoke the authentication.
+        - If the user is not found or has no associated token, an error message is returned.
+
+    Example Success Response:
+    ```
+    HTTP 200 OK
+    {
+        "message": "User successfully logged out. Token revoked."
+    }
+    ```
+
+    Example Error Response:
+    ```
+    HTTP 400 BAD REQUEST
+    {
+        "Error": "Could not find user with email: user@example.com"
+    }
+    ```
+    """
+
     def get(self, request):
         try:
             Token.objects.get(user=request.user).delete()
