927cc180d2e4eb6eaba235758f6abf5db2d2456c
jam4li <ziigm4@gmail.com>
2023-07-24 20:25:53 +0330

    Add BackupAdmin with disabled add and delete permissions

--- apps/backup/admin.py
+++ apps/backup/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
+from apps.base.admin import BaseAdmin
+
+from .models import Backup
+
 # Register your models here.
+
+
+class BackupAdmin(BaseAdmin):
+    fields = (
+        'name',
+        'status',
+    ) + BaseAdmin.fields
+
+    list_display = (
+        'name',
+        'status',
+        'created_at',
+    )
+
+    readonly_fields = (
+        'name',
+        'status',
+    ) + BaseAdmin.readonly_fields
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request):
+        return False
+
+
+admin.site.register(Backup, BackupAdmin)
